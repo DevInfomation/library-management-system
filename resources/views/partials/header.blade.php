2a39217ebad4b00894d9a7924cd24fb16edf0254
DevInfo <devinformationproject@gmail.com>
2025-03-02 19:06:38 +0100

    Creating the top part with the name of the project and the login/register buttons

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,10 @@
-<div class="border border-gray-400 bg-gray-500 overflow-visible p-6">
-    <div class="space-y-2">
-        <h1 class="text-white">Test test test test</h1>
+<div class="border border-gray-400 bg-gray-500 overflow-visible p-6 flex justify-between">
+    <div class="">
+        <h2 class="text-white">Library Management System</h2>
+    </div>
+    <div class="flex">
+        <h2 class="mr-2"><a href="#">Login</a></h2>
+        <h2 class="mr-2"><a href="#">Admin login</a></h2>
+        <h2><a href="#">Register</a></h2>
     </div>
 </div>
