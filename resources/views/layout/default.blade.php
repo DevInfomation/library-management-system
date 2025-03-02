3bda8985766a6b4aa2d82368f75de29d50b4285d
DevInfo <devinformationproject@gmail.com>
2025-03-02 19:07:02 +0100

    Changing the long name to global-wallpaper and just adding the rest in the css file

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../../css">
+    <link rel="stylesheet" href="../../css/app.css">
     @vite('resources/css/app.css')
     <title>Library Management System Project</title>
 </head>
-<body class="min-h-screen bg-fixed bg-cover bg-no-repeat bg-gray-800" style="background-image:url('{{asset('images/library_wallpaper.jpg')}}')">
+<body class="min-h-screen global-wallpaper">
 
     <header>
         @include('partials.header')
