cde0cbe4c03eb51af15e63df165d3f35a49ddb99
DevInfo <devinformationproject@gmail.com>
2025-03-03 23:03:59 +0100

    Creating the register file

---
+++ resources/views/register.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.default')
+@section('content')
+    <div class="border rounded-sm bg-gray-200 bg-opacity-90 min-h-[400px]">
+        <div class="mx-[40rem] mt-12">
+            <form action="/register" method="POST" class="flex flex-col">
+                @csrf
+                <h1 class="text-2xl text-center font-semibold">Register User</h1>
+                <label class="mb-2">Email ID:</label>
+                <input type="text" name="email" class="border rounded-md p-1 mb-2">
+                <label class="mb-2">Password:</label>
+                <input type="password" name="password" class="border rounded-md p-1 mb-2">
+                <div class="flex mt-3">
+                    <button class="mr-2 border rounded-md bg-[#037bf7] p-2 text-white">Register</button>
+                    <button class="mr-2 border rounded-md bg-[#037bf7] p-2 text-white"><a href="{{route('home')}}">Cancel</a></button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
