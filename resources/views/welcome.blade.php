0baa6fbd358a3c9f792477a98fb41a544ac9ed01
DevInfo <devinformationproject@gmail.com>
2025-03-03 20:33:31 +0100

    It's becoming more clear by the minute

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     
     <div class="border rounded-sm bg-gray-200 min-h-[400px]">
-        <div class=" flex">
+        <div class="flex w-10/12">
             <div class="w-1/3 ml-10">
                 <h3 class="mt-2 text-xl font-semibold">Today's Quote</h3>
                 <p class="font-semibold">"There is more treasure in books than in all<br>the pirate's loot on Treasure Island"</p>
@@ -23,17 +23,17 @@
                     <li>Free Electricity</li>
                 </ul>
             </div>
-            <form action="login" method="POST" class="flex flex-1">
+            <form action="login" method="POST" class="flex flex-1 mt-5">
                 @csrf
                 <div class="flex flex-col flex-1">
                     <h1 class="text-xl font-semibold text-center">User Login Form</h1>
-                    <label class="text-left">Email ID:</label>
-                    <input type="text" name="email">
-                    <label class="text-left" >Password:</label>
-                    <input type="password" name="password">
+                    <label class="text-left mb-2">Email ID:</label>
+                    <input type="text" name="email" class="border rounded-md p-1 mb-2">
+                    <label class="text-left mb-2">Password:</label>
+                    <input type="password" name="password" class="border rounded-md p-1">
                     <div class="flex mt-2">
                         <button class="mr-2 border rounded-md bg-[#037bf7] p-2">Login</button>
-                        <p class="mt-2"><a href="#">Sign up now!!!</a></p>
+                        <p class="mt-2 text-[#037bf7]"><a href="#">Sign up now!!!</a></p>
                     </div>
                 </div>
             </form>
