dcb05e17d0bc4cd5c2d8613a1cdeee24ece60171
DevInfo <devinformationproject@gmail.com>
2025-03-03 20:39:27 +0100

    Making the title bigger

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <div class="border border-gray-400 bg-gray-500 overflow-visible p-6 flex justify-between">
     <div class="">
-        <h2 class="text-white">Library Management System</h2>
+        <h2 class="text-white text-xl">Library Management System</h2>
     </div>
     <div class="flex">
         <h2 class="mr-2"><a href="#">Login</a></h2>
