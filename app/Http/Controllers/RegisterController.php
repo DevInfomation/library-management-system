e2b4449319d76142e5b689400d1b9e8c1af41010
Stefan <stefan.milkovski@generadevelopment.com>
2025-03-04 14:36:30 +0100

    Pushing this till I get home

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
@@ -12,7 +13,10 @@ class RegisterController extends Controller
             'email' => ['required', Rule::unique('users', 'email')],
             'password' => ['required', 'min:3', 'max:30'],
         ]);
-
         
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+
+        $registeredUser = User::create($incomingFields);
+        auth()->login($registeredUser);
     }
 }
