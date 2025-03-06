f45384a6f06fe5121f1442373d2dcef5c6bbfc49
Stefan <stefan.milkovski@generadevelopment.com>
2025-03-06 15:45:56 +0100

    Adding the logout styel and also issued books. Best time I think to start to work on the books

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,34 @@
 @extends('layout.default')
 @section('content')
-    <div>
-        <div class="flex border-2 rounded-md border-black">
-            <form action="{{route('logout')}}" method="POST" class="bg-white">
-                @csrf
-                <button class="text-white border bg-sky-400">Logout</button>
-            </form>
+    <div class="flex">
+        <div class="border-4 rounded-md border-black w-1/6 bg-white ml-4 mt-4 min-h-[8rem]">
+            <div class="bg-gray-400 p-2">
+                <p>Logout</p>
+            </div>
+            <div class="p-2">
+                <div>
+                    <p>Do you wish to logout?</p>
+                </div>
+                <form action="{{route('logout')}}" method="POST" class="bg-white">
+                    @csrf
+                    <button class="text-white border rounded-md bg-green-400 p-1 mt-4">Logout</button>
+                </form>
+            </div>
+        </div>
+        <div class="border-4 rounded-md border-black w-1/6 bg-white ml-4 mt-4 min-h-[8rem]">
+            <div class="bg-gray-400 p-2">
+                <p>Books Issued</p>
+            </div>
+            <div class="p-2">
+                <div>
+                    {{-- Keep the 0 for now and we will update with the database soon --}}
+                    <p>Issued books: 0</p> 
+                </div>
+                <form action="" method="POST" class="bg-white">
+                    @csrf
+                    <button class="text-white border rounded-md bg-green-400 p-1 mt-4">View Issued Books</button>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
