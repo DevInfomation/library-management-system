1d0df09abd3c87305996097e6e6d47deb8cb2da0
Stefan <stefan.milkovski@generadevelopment.com>
2025-03-06 14:49:31 +0100

    adding a logout button to the library

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,11 @@
 @extends('layout.default')
 @section('content')
-    
+    <div>
+        <div class="flex border-2 rounded-md border-black">
+            <form action="{{route('logout')}}" method="POST" class="bg-white">
+                @csrf
+                <button class="text-white border bg-sky-400">Logout</button>
+            </form>
+        </div>
+    </div>
 @endsection
