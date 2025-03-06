b86c41ee8484f27073ee4b76e2b23482c3d341e0
DevInfo <devinformationproject@gmail.com>
2025-03-06 08:11:59 +0100

    Adding changes to the header to make guests see one thing while making logged in users see another

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,9 +2,17 @@
     <div class="">
         <h2 class="text-white text-xl">Library Management System</h2>
     </div>
-    <div class="flex">
-        <h2 class="mr-2"><a href="#">Login</a></h2>
-        <h2 class="mr-2"><a href="#">Admin login</a></h2>
-        <h2><a href="{{route('register')}}">Register</a></h2>
-    </div>
+    @if (Auth::guest())
+        <div class="flex">
+            <h2 class="mr-2"><a href="#">Login</a></h2>
+            <h2 class="mr-2"><a href="#">Admin login</a></h2>
+            <h2><a href="{{route('register')}}">Register</a></h2>
+        </div>
+    @else
+        <div class="flex">
+            <h2 class="mr-2">{{$user->name}}</h2>
+            <h2 class="mr-2">{{$user->email}}</h2>
+            {{-- <h2></h2> --}}
+        </div>    
+    @endif
 </div>
