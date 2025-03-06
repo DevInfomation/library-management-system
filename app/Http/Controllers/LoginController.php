ea8c45cc8aedd3733f5fbdd37ecdb7ed11b5fbf5
DevInfo <devinformationproject@gmail.com>
2025-03-06 07:44:38 +0100

    Creating the login and the logout functions

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request) {
+        $incomingFields = $request->validate([
+            'loginname' => 'required',
+            'loginpassword' => 'required',
+        ]);
+
+        if (auth()->attempt(['name' => $incomingFields['loginname'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+        }
+
+        return redirect()->route('dashboard');
+    }
+
+    public function logout() {
+        auth()->logout();
+        return redirect()->route('home');
+    }
 }
