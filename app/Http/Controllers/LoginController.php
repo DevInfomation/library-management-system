085804274df567868a3b3de6985b8090c80bbaf1
DevInfo <devinformationproject@gmail.com>
2025-03-15 14:11:13 +0100

    Making the name nullable so I can have it just in case for the report to get the id I need

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,6 +8,7 @@ class LoginController extends Controller
 {
     public function login(Request $request) {
         $incomingFields = $request->validate([
+            'name' => 'nullable|string',
             'email' => 'required',
             'password' => 'required',
         ]);
