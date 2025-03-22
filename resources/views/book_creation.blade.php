143e59f07d3216e6716e841e681ed49ce7a4fe67
DevInfo <devinformationproject@gmail.com>
2025-03-22 15:11:34 +0100

    Creating the whole look of the book form

--- resources/views/book_creation.blade.php
+++ resources/views/book_creation.blade.php
@@ -1,4 +1,25 @@
 @extends('layout.default')
 @section('content')
-     
+    <div class="h-screen flex items-center">
+        <form action="" method="POST" class="mx-auto w-1/3 border-4 rounded-md border-black bg-white p-4">
+            @csrf
+            <div class="flex flex-col">
+                <label class="mb-1">Book title:</label>
+                <input type="text" name="title" class="border rounded-md p-1.5 mb-2" placeholder="Enter book title...">
+                <label class="mb-1">Book author:</label>
+                <input type="text" name="title" class="border rounded-md p-1.5 mb-2" placeholder="Enter book title...">
+                <label class="mb-1">Book genre:</label>
+                <input type="text" name="title" class="border rounded-md p-1.5 mb-2" placeholder="Enter book title...">
+                <label class="mb-1">Book category:</label>
+                <input type="text" name="title" class="border rounded-md p-1.5 mb-2" placeholder="Enter book title...">
+                <label class="mb-1">Book published year:</label>
+                <input type="text" name="title" class="border rounded-md p-1.5 mb-2" placeholder="Enter book title...">
+                <label class="mb-1">Book price:</label>
+                <input type="text" name="title" class="border rounded-md p-1.5 mb-2" placeholder="Enter book title...">
+                <label class="mb-1">Book copies:</label>
+                <input type="text" name="title" class="border rounded-md p-1.5 mb-2" placeholder="Enter book title...">
+                <button class="text-white border rounded-md bg-green-400 p-1 mt-4">Create Your Book</button>
+            </div>
+        </form>
+    </div>
 @endsection
