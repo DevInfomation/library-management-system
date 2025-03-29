9831aa7e3f2e2425367768a123a60b36a32df999
DevInfo <devinformationproject@gmail.com>
2025-03-29 16:01:47 +0100

    Adding some new boxes and also a script at the end for making a search for a book

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -54,13 +54,25 @@
                 <p class="mt-3 text-center text-3xl">{{$count}}</p>
             </div>
         </div>
+        <div class="border-4 rounded-md border-black w-1/6 bg-white ml-4 mt-4 min-h-[8rem]">
+            <div class="bg-gray-400 p-2">
+                <p>Return a Book</p>
+            </div>
+            <div class="p-2">
+                <div>
+                    {{-- This will be empty till I have a system for returning books --}}
+                    <p>Return a book here:</p> 
+                </div>
+                <a href="#"><button class="text-white border rounded-md bg-green-400 p-1 mt-4">Return a book</button></a>
+            </div>
+        </div>
     </div>
     <div class="flex justify-evenly">
-        <div class="border-4 rounded-md border-black w-1/3 bg-white ml-4 mt-12 min-h-[8rem]">
+        <div class="border-4 rounded-md border-black w-1/3 bg-white ml-4 mt-12 min-h-[10rem]">
             <div class="bg-gray-400 p-2">
                 <p class="text-center">Search for a book</p>
             </div>
-            <div class='max-w-md mx-auto'>
+            <div class='max-w-md mx-auto flex flex-col'>
                 <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
                     <div class="grid place-items-center h-full w-12 text-gray-300">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -71,9 +83,24 @@
                         class="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
                         type="text"
                         id="search"
-                        placeholder="Search for a book..." /> 
+                        placeholder="Search for a book..."> 
                 </div>
+                <button class="text-white border rounded-md bg-green-400 p-1 mt-4" onclick="hideSearchedBooks()">Search</button>
             </div>
         </div>
     </div>
+    <div class="flex justify-evenly" id="searched-books" style="display: none;">
+        <div class="border-4 rounded-md border-black w-1/3 bg-white ml-4 mt-12 min-h-[10rem]">
+            <div class="bg-gray-400 p-2">
+                <p class="text-center">The Searched Book</p>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function hideSearchedBooks() {
+            const hide = document.getElementById("searched-books");
+            hide.style.display = '';
+        }
+    </script>
 @endsection
