21c4fe9e319e60040cb7f70aafef45f9106050ba
DevInfo <devinformationproject@gmail.com>
2025-03-29 14:48:30 +0100

    removing the required for the moment and also changing the return to redirect to the dashboard

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,16 +11,16 @@ class BookController extends Controller
     public function store(Request $request) {
         $validated = $request->validate([
             'isbn' => ['sometimes', new Isbn13Rule],
-            'title' => ['required', 'string'],
-            'author' => ['required', 'string'],
-            'genre' => ['required', 'string'],
-            'category' => ['required', 'string'],
-            'published year' => ['required', 'integer'],
-            'price' => ['required', 'integer'],
-            'copies available' => ['required', 'integer'],
+            'title' => 'string',
+            'author' => 'string',
+            'genre' => 'string',
+            'category' => 'string',
+            'published-year' => 'integer',
+            'price' => 'integer',
+            'copies-available' => 'integer',
         ]);
 
 
-        return Book::create($validated);
+        return redirect()->route('dashboard');
     }
 }
